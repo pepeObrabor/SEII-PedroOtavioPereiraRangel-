d016c671f6d93b1bdc0ed564d46abd54950a3d76
Pedro Otavio <peperangel2000@gmail.com>
2023-08-17 13:24:11 -0300

    semana 02

---
+++ semana02/exercicio04/py03.py
@@ -0,0 +1,44 @@
+
+
+num = 3.14
+
+print(type(num))
+
+print(3 + 2)
+print(3 - 2)
+print(3 * 2)
+print(3 / 2)
+print(3 // 2)
+print(3 ** 2)
+print(3 % 2)
+print(3 * 2 + 1)
+print(3 * (2 + 1))
+
+numn = 1
+
+numn =  numn + 1
+
+print(numn)
+
+print(abs(-3))
+
+print(round(3.55, 1))
+
+num_1 = 3
+num_2 = 2
+
+print(num_1 == num_2)
+print(num_1 != num_2)
+print(num_1 > num_2)
+print(num_1 < num_2)
+print(num_1 >= num_2)
+print(num_1 <= num_2)
+
+num_3 = '100'
+num_4 = '200'
+
+num_3b = int(num_3)
+num_4b = int(num_4)
+
+print(num_3 + num_4)
+print(num_3b + num_4b)
